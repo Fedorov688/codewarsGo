b9e24e9b39f81a256f3d39fef814020c6d62eef6
Mordok <fedorov688@gmail.com>
2021-03-05 22:49:47 +0300

    ip_validation done

--- code/ip_validation/ip_validation.go
+++ code/ip_validation/ip_validation.go
@@ -1,6 +1,28 @@
 package ip_validation
 
-func Is_valid_ip(ip string) bool {
+import (
+	"strconv"
+	"strings"
+)
 
-	return false
+func Is_valid_ip(ip string) bool {
+	var octet = strings.Split(ip, ".")
+	if len(octet) != 4 {
+		return false
+	}
+	for _, values := range octet {
+		if s, err := strconv.Atoi(values); err == nil {
+			if s > 255 {
+				return false
+			}
+		}
+		for key, value := range values {
+			if value < 48 || value > 57 {
+				return false
+			} else if key == 0 && value == 48 && len(values) > 1 {
+				return false
+			}
+		}
+	}
+	return true
 }
